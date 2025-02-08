155cd92834beaa34c6fb68023c41ea4d8e5f20e5
joao <joaovargas040@gmail.com>
2025-02-08 19:20:53 -0300

    Adding classes generated form starUML

---
+++ code/menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Interface1 import Interface1
+
+
+class Menu(Interface1):
+    def __init__(self):
+        self.window = None
+
+    def run(self, ):
+        pass
